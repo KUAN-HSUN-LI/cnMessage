e82c1ce9b1818ec4e8dc0df11d0619e125129cc8
KUAN-HSUN-LI <b06209027@ntu.edu.tw>
2020-01-18 00:28:37 +0800

    v1 finish for deploy

--- src/index.js
+++ src/index.js
@@ -13,20 +13,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// const WS = window.location.origin.replace('http', 'ws');
+const WS = window.location.origin.replace('http', 'ws');
 
 const wsLink = new WebSocketLink({
 	// uri: 'ws://localhost:4000/',
-	uri: '/graphql',
-	//uri: WS,
+	uri: WS,
 	options: { reconnect: true },
 	// credentials: 'include',
 });
 
 const upLink = new createUploadLink({
-	uri: '/graphql',
 	// uri: 'http://localhost:4000/',
-	//uri: '/',
+	uri: '/',
 	// credentials: 'include',
 });
 
