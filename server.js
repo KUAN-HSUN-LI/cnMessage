7acccf85f819507e983fed771396717bd0f165cc
KUAN-HSUN-LI <b06209027@ntu.edu.tw>
2020-01-18 03:00:03 +0800

    test

---
+++ server.js
@@ -0,0 +1,45 @@
+import { GraphQLServer, PubSub } from 'graphql-yoga';
+import { startDB, models } from './src/server/db';
+import Subscription from './src/server/graphql/resolvers/Subscription';
+import Mutation from './src/server/graphql/resolvers/Mutation';
+import Query from './src/server/graphql/resolvers/Query';
+
+const db = startDB({ URL: 'mongodb+srv://jingch:jingch1213@cluster0-tninn.gcp.mongodb.net/test?retryWrites=true&w=majority' });
+
+const pubsub = new PubSub();
+
+const context = {
+	pubsub,
+	db,
+	models,
+};
+
+const server = new GraphQLServer({
+	typeDefs: `${__dirname}/src/server/graphql/schema.graphql`,
+	resolvers: {
+		Query,
+		Mutation,
+		Subscription,
+	},
+	context,
+});
+
+if (process.env.NODE_ENV === 'production') {
+	const root = path.join(__dirname, 'build');
+	server.express.use(express.static(root));
+	server.express.get('*', (req, res) => {
+		res.sendFile('index.html', { root });
+	});
+}
+
+// const opts = {
+//     port: process.env.PORT || 4000,
+//     cors: {
+//       credentials: true,
+//       origin: ['http://localhost:3000','http://localhost:3001']
+//     },
+// };
+
+server.start({ port: process.env.PORT | 4000 }, () => {
+	console.log(`The server is up on port ${process.env.PORT | 4000}!`);
+});
