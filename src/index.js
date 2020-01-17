94f6211825788882b4fc16261b214f056b85e822
KUAN-HSUN-LI <b06209027@ntu.edu.tw>
2020-01-18 02:24:31 +0800

    v1 finish for deploy

--- src/index.js
+++ src/index.js
@@ -13,19 +13,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// const WS = window.location.origin.replace('http', 'ws');
+const WS = window.location.origin.replace('http', 'ws');
 
 const wsLink = new WebSocketLink({
 	// uri: 'ws://localhost:4000/',
-	uri: '/',
+	uri: WS,
 	options: { reconnect: true },
-	credentials: 'include',
+	// credentials: 'include',
 });
 
 const upLink = new createUploadLink({
 	// uri: 'http://localhost:4000/',
 	uri: '/',
-	credentials: 'include',
+	// credentials: 'include',
 });
 
 /*
--- src/server/index.js
+++ src/server/index.js
@@ -24,22 +24,6 @@ const server = new GraphQLServer({
 	context,
 });
 
-server.express.use(
-	session({
-		name: 'jackson',
-		secret: `some-random-secret-here`,
-		resave: true,
-		saveUninitialized: true,
-		cookie: {
-			secure: process.env.NODE_ENV === 'production',
-			maxAge: ms('1d'),
-		},
-	})
-);
-
-server.express.set('trust proxy', true);
-server.express.use(bodyParser({ limit: '16mb' }));
-
 if (process.env.NODE_ENV === 'production') {
 	const root = path.join(__dirname, 'build');
 	server.express.use(express.static(root));
@@ -48,16 +32,14 @@ if (process.env.NODE_ENV === 'production') {
 	});
 }
 
-const opts = {
-	port: process.env.PORT || 4000,
-	cors: {
-		credentials: true,
-		origin: ['http://localhost:3000', 'http://localhost:3001'],
-	},
-};
+// const opts = {
+//     port: process.env.PORT || 4000,
+//     cors: {
+//       credentials: true,
+//       origin: ['http://localhost:3000','http://localhost:3001']
+//     },
+// };
 
-server.start(opts, () => console.log(`Server is running on http://localhost:${opts.port}`));
-
-// server.start({ port: process.env.PORT | 4000 }, () => {
-// 	console.log(`The server is up on port ${process.env.PORT | 4000}!`);
-// });
+server.start({ port: process.env.PORT | 4000 }, () => {
+	console.log(`The server is up on port ${process.env.PORT | 4000}!`);
+});
