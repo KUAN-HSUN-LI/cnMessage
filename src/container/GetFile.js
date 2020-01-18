87905719dbc9b772634647186c58607d22b83288
KUAN-HSUN-LI <b06209027@ntu.edu.tw>
2020-01-18 23:34:30 +0800

    test upload file to heroku

--- src/container/GetFile.js
+++ src/container/GetFile.js
@@ -6,23 +6,23 @@ import fileDownload from 'react-file-download';
 const GetFile = props => {
 	const { name, msgBoxId } = props;
 	// const [scribe, setScribe] = useState(false);
-	// const test = useSubscription(FILE_SUBSCRIPTION, { variables: { msgBoxId: msgBoxId, reciever: name } });
+	const test = useSubscription(FILE_SUBSCRIPTION, { variables: { msgBoxId: msgBoxId, reciever: name } });
 
-	// // if (test.data && !scribe) {
-	// // 	var { stream, filename, mimetype, encoding } = test.data.file.data;
-	// // 	var output = Buffer.from(stream, 'base64');
-	// // 	console.log(test);
-	// // 	fileDownload(output, filename, mimetype);
-	// // 	setScribe(true);
-	// // }
+	// if (test.data && !scribe) {
+	// 	var { stream, filename, mimetype, encoding } = test.data.file.data;
+	// 	var output = Buffer.from(stream, 'base64');
+	// 	console.log(test);
+	// 	fileDownload(output, filename, mimetype);
+	// 	setScribe(true);
+	// }
 
-	// useEffect(() => {
-	// 	if (test.data) {
-	// 		var { stream, filename, mimetype } = test.data.file.data;
-	// 		var output = Buffer.from(stream, 'base64');
-	// 		fileDownload(output, filename, mimetype);
-	// 	}
-	// }, [test.data]);
+	useEffect(() => {
+		if (test.data) {
+			var { stream, filename, mimetype } = test.data.file.data;
+			var output = Buffer.from(stream, 'base64');
+			fileDownload(output, filename, mimetype);
+		}
+	}, [test.data]);
 	return <></>;
 };
 
