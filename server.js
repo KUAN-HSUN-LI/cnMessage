92981e00431205c958757163774c2480a6936af2
KUAN-HSUN-LI <b06209027@ntu.edu.tw>
2021-01-23 20:27:06 +0800

    repair style and fix bugs

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-require('dotenv').config();
-import { GraphQLServer, PubSub } from 'graphql-yoga';
-import { startDB, models } from './server/db';
-import Subscription from './server/graphql/resolvers/Subscription';
-import Mutation from './server/graphql/resolvers/Mutation';
-import Query from './server/graphql/resolvers/Query';
-import path from 'path';
-import express from 'express';
+require("dotenv").config()
+import { GraphQLServer, PubSub } from "graphql-yoga"
+import { startDB, models } from "./server/db"
+import Subscription from "./server/graphql/resolvers/Subscription"
+import Mutation from "./server/graphql/resolvers/Mutation"
+import Query from "./server/graphql/resolvers/Query"
+import path from "path"
+import express from "express"
 
-const db = startDB({ URL: 'mongodb+srv://jingch:jingch1213@cluster0-tninn.gcp.mongodb.net/test?retryWrites=true&w=majority' });
+const db = startDB({ URL: process.env.MONGODB_URL })
 
-const pubsub = new PubSub();
+const pubsub = new PubSub()
 
 const context = {
-	pubsub,
-	db,
-	models,
-};
+  pubsub,
+  db,
+  models,
+}
 
 const server = new GraphQLServer({
-	typeDefs: `${__dirname}/server/graphql/schema.graphql`,
-	resolvers: {
-		Query,
-		Mutation,
-		Subscription,
-	},
-	context,
-});
+  typeDefs: `${__dirname}/server/graphql/schema.graphql`,
+  resolvers: {
+    Query,
+    Mutation,
+    Subscription,
+  },
+  context,
+})
 
-if (process.env.NODE_ENV === 'production') {
-	const root = path.join(__dirname, 'build');
-	server.express.use(express.static(root));
-	server.express.get('*', (req, res) => {
-		res.sendFile('index.html', { root });
-	});
+if (process.env.NODE_ENV === "production") {
+  const root = path.join(__dirname, "build")
+  server.express.use(express.static(root))
+  server.express.get("*", (req, res) => {
+    res.sendFile("index.html", { root })
+  })
 }
 
 server.start({ port: process.env.PORT || 4000 }, () => {
-	console.log(`The server is up on port ${process.env.PORT | 4000}!`);
-});
+  console.log(`The server is up on port ${process.env.PORT | 4000}!`)
+})
--- src/container/ChatRoom.js
+++ src/container/ChatRoom.js
@@ -1,128 +1,138 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './ChatRoom.css';
-import { useMutation, useSubscription } from 'react-apollo';
-import Message from '../component/Message';
-import GetFile from './GetFile';
-import AddFriend from '../component/AddFriend';
+import React, { useState, useEffect, useRef } from "react"
+import "./ChatRoom.css"
+import { useMutation, useSubscription } from "react-apollo"
+import Message from "../component/Message"
+import GetFile from "./GetFile"
+import AddFriend from "../component/AddFriend"
 
-import { CREATE_MSG_MUTATION, UPLOAD_FILE_MUTATION, FRIEND_SUBSCRIPTION } from '../graphql';
+import {
+  CREATE_MSG_MUTATION,
+  UPLOAD_FILE_MUTATION,
+  FRIEND_SUBSCRIPTION,
+} from "../graphql"
 
 const ChatRoom = () => {
-	const name = localStorage.getItem('name');
-	const [msgBoxId, setMsgBoxId] = useState(null);
-	const [friend, setFriend] = useState(null);
-	const newMsg = useRef(null);
-	const [friends, setFriends] = useState(JSON.parse(localStorage.getItem('friends')));
-	const [createMsgMutation] = useMutation(CREATE_MSG_MUTATION);
-	const [uploadFileMutation] = useMutation(UPLOAD_FILE_MUTATION);
+  const name = localStorage.getItem("name")
+  const [msgBoxId, setMsgBoxId] = useState(null)
+  const [friend, setFriend] = useState(null)
+  const newMsg = useRef(null)
+  const [friends, setFriends] = useState(
+    JSON.parse(localStorage.getItem("friends"))
+  )
+  const [createMsgMutation] = useMutation(CREATE_MSG_MUTATION)
+  const [uploadFileMutation] = useMutation(UPLOAD_FILE_MUTATION)
 
-	const newFriend = useSubscription(FRIEND_SUBSCRIPTION, { variables: { name: name } });
+  const newFriend = useSubscription(FRIEND_SUBSCRIPTION, {
+    variables: { name: name },
+  })
 
-	const handleSubmit = e => {
-		e.preventDefault();
-		if (newMsg.current.value === '') return;
-		createMsgMutation({
-			variables: {
-				messageBoxId: msgBoxId,
-				author: name,
-				body: newMsg.current.value,
-			},
-		}).then((newMsg.current.value = ''));
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (newMsg.current.value === "") return
+    createMsgMutation({
+      variables: {
+        messageBoxId: msgBoxId,
+        author: name,
+        body: newMsg.current.value,
+      },
+    }).then((newMsg.current.value = ""))
+  }
 
-	const handleUploadFile = e => {
-		const file = e.target.files[0];
-		uploadFileMutation({
-			variables: { file: file, msgBoxId: msgBoxId, reciever: friend },
-		});
-	};
-	useEffect(() => {
-		if (newFriend.data) {
-			setFriends(friends => friends.concat(newFriend.data.friend.data));
-		}
-	}, [newFriend.data]);
+  const handleUploadFile = (e) => {
+    const file = e.target.files[0]
+    uploadFileMutation({
+      variables: { file: file, msgBoxId: msgBoxId, reciever: friend },
+    })
+  }
+  useEffect(() => {
+    if (newFriend.data) {
+      setFriends((friends) => friends.concat(newFriend.data.friend.data))
+    }
+  }, [newFriend.data])
 
-	useEffect(() => {
-		localStorage.setItem('friends', JSON.stringify(friends));
-	}, [friends]);
+  useEffect(() => {
+    localStorage.setItem("friends", JSON.stringify(friends))
+  }, [friends])
 
-	useEffect(() => {
-		var current = document.getElementById(friend);
-		if (current) {
-			current.classList.remove('Friend_list_black');
-			current.classList.add('Friend_list_white');
-			return () => {
-				current.classList.remove('Friend_list_white');
-				current.classList.add('Friend_list_black');
-			};
-		}
-	}, [friend]);
+  useEffect(() => {
+    var current = document.getElementById(friend)
+    if (current) {
+      current.classList.remove("Friend_list_black")
+      current.classList.add("Friend_list_white")
+      return () => {
+        current.classList.remove("Friend_list_white")
+        current.classList.add("Friend_list_black")
+      }
+    }
+  }, [friend])
 
-	useEffect(() => {
-		console.log(newMsg.current.value);
-	});
+  return (
+    <div className="ChatRoom">
+      <div className="Sidebar">
+        <div className="snow-container">
+          <div className="snow middleground"></div>
+          <div className="snow middleground layered"></div>
+          <div className="snow background"></div>
+          <div className="snow background layered"></div>
+        </div>
+        <p className="left-title">
+          <i className="fa fa-address-book"></i>Directory
+        </p>
 
-	return (
-		<div className="ChatRoom">
-			<div className="Sidebar">
-				<div className="snow-container">
-					<div className="snow middleground"></div>
-					<div className="snow middleground layered"></div>
-					<div className="snow background"></div>
-					<div className="snow background layered"></div>
-				</div>
-				<p className="left-title">
-					<i className="fa fa-address-book"></i>通訊錄
-				</p>
+        <AddFriend name={name} />
+        {friends.map((d, idx) => (
+          <div
+            className="Friend_list Friend_list_black"
+            key={idx}
+            onClick={() => {
+              setMsgBoxId(d.messageBox)
+              setFriend(d.name)
+            }}
+            id={d.name}
+          >
+            {d.name}
+          </div>
+        ))}
+      </div>
+      <div className="Header">
+        <p className="right-title">
+          <i className="fa fa-commenting-o"></i>Message
+        </p>
+      </div>
+      {msgBoxId ? (
+        <>
+          <Message messageBox={msgBoxId} name={name}></Message>
+          <GetFile name={name} msgBoxId={msgBoxId}></GetFile>
+        </>
+      ) : (
+        <div className="Body" id="Body">
+          <h1>Choose a friend</h1>
+        </div>
+      )}
 
-				<AddFriend name={name} />
-				{friends.map((d, idx) => (
-					<div
-						className="Friend_list Friend_list_black"
-						key={idx}
-						onClick={() => {
-							setMsgBoxId(d.messageBox);
-							setFriend(d.name);
-						}}
-						id={d.name}>
-						{d.name}
-					</div>
-				))}
-			</div>
-			<div className="Header">
-				<p className="right-title">
-					<i className="fa fa-commenting-o"></i>Message
-				</p>
-			</div>
-			{msgBoxId ? (
-				<>
-					<Message messageBox={msgBoxId} name={name}></Message>
-					<GetFile name={name} msgBoxId={msgBoxId}></GetFile>
-				</>
-			) : (
-				<div className="Body" id="Body">
-					<h1>Choose a friend</h1>
-				</div>
-			)}
+      <div className="Footer">
+        <label htmlFor="file-upload" className="custom-file-upload">
+          <i className="fa fa-folder"></i> File
+        </label>
+        <input
+          id="file-upload"
+          type="file"
+          onChange={(e) => handleUploadFile(e)}
+        />
 
-			<div className="Footer">
-				<label htmlFor="file-upload" className="custom-file-upload">
-					<i className="fa fa-folder"></i> File
-				</label>
-				<input id="file-upload" type="file" onChange={e => handleUploadFile(e)} />
+        <textarea
+          className="textbox"
+          type="text"
+          onKeyPress={(e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+              handleSubmit(e)
+            }
+          }}
+          ref={newMsg}
+        ></textarea>
+      </div>
+    </div>
+  )
+}
 
-				<textarea
-					className="textbox"
-					type="text"
-					onKeyPress={e => {
-						if (e.key === 'Enter' && !e.shiftKey) {
-							handleSubmit(e);
-						}
-					}}
-					ref={newMsg}></textarea>
-			</div>
-		</div>
-	);
-};
-
-export default ChatRoom;
+export default ChatRoom
